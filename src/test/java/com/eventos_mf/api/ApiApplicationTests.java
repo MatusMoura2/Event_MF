b6f63cd043ee923d2483195933c2f1105894c48e
MatusMoura2 <matus.moura333x2@gmail.com>
2025-09-13 23:40:32 -0300

    criaçao das entidades

---
+++ src/test/java/com/eventos_mf/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventos_mf.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
